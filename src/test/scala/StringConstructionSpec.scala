6f5305039b1d32ce9dcae8ad9c0005238a506391
Richard Goulter <richard.goulter@gmail.com>
2015-02-01 11:57:15 +0800

    Add ScalaTest FlatSpec for String Construction.

---
+++ src/test/scala/StringConstructionSpec.scala
@@ -0,0 +1,32 @@
+import org.scalatest._
+
+class StringConstructionSpec extends FlatSpec {
+
+  "String Construction" should "describe simple declarations" in {
+    val input = "int x;";
+    val expected = PrimitiveType("x", "int");
+    val actual = StringConstruction.getCTypeOf(input);
+
+    assertResult(expected)(actual);
+  }
+  
+  it should "describe 1D array declarations" in {
+    val input = "int x[4];";
+    val expected = ArrayType("x", "x_0", "4", PrimitiveType("x[x_0]", "int"));
+    val actual = StringConstruction.getCTypeOf(input);
+
+    assertResult(expected)(actual);
+  }
+
+  it should "describe multi-dimension array declarations" in {
+    val input = "int x[3][4][5];";
+    val arrayPrim = PrimitiveType("x[x_0][x_1][x_2]", "int")
+    val arrayDim2 = ArrayType("x[x_0][x_1]", "x_2", "5", arrayPrim);
+    val arrayDim1 = ArrayType("x[x_0]", "x_1", "4", arrayDim2);
+    val expected = ArrayType("x", "x_0", "3", arrayDim1);
+    val actual = StringConstruction.getCTypeOf(input);
+
+    assertResult(expected)(actual);
+  }
+
+}
