3d9fdb7d5ab32387fb26e9859aee4392d3c8517c
Richard Goulter <richard.goulter@gmail.com>
2015-02-15 21:18:26 +0800

    Can output a dereference of a pointer.

--- src/test/scala/edu/nus/worksheet/instrumentor/test/WorksheetifySpec.scala
+++ src/test/scala/edu/nus/worksheet/instrumentor/test/WorksheetifySpec.scala
@@ -68,4 +68,45 @@ int main(int argc, char* argv) { // Line 03
       case None => fail("No output was given.");
     }
   }
+
+  it should "be able to dereference a valid pointer" in {
+    val inputProgram = """#include <stdio.h>
+
+int main(int argc, char* argv) { // Line 03
+  int x = 5;
+  int *p;
+  p = &x;
+}""";
+    val inputLines = inputProgram.lines.toList;
+    
+    val wsOutput = new WorksheetOutput();
+    Worksheetify.processWorksheet(inputLines, wsOutput);
+    val wsOutputStr = wsOutput.generateWorksheetOutput(inputLines); // block until done.
+
+    wsOutput.outputPerLine.get(6) match {
+      case Some(Seq(actual)) => assert(actual.charAt(actual.length() - 1) == '5', actual);
+      case None => fail("No output was given.");
+    }
+  }
+
+  it should "be able to dereference an invalid pointer" in {
+    val inputProgram = """#include <stdio.h>
+
+int main(int argc, char* argv) { // Line 03
+  int x = 5;
+  int *p;
+  p = 0;
+}""";
+    val inputLines = inputProgram.lines.toList;
+    
+    val wsOutput = new WorksheetOutput();
+    Worksheetify.processWorksheet(inputLines, wsOutput);
+    val wsOutputStr = wsOutput.generateWorksheetOutput(inputLines); // block until done.
+
+    wsOutput.outputPerLine.get(6) match {
+      // If we couldn't dereference pointers, we'd get a segfault.
+      case Some(Seq(actual)) => assert(actual.toLowerCase().indexOf("seg") < 0, actual);
+      case None => fail("No output was given.");
+    }
+  }
 }
