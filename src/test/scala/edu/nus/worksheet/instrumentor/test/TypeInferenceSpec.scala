c3f3fba7404ed47ed3ef1f1a6b429ce079fbcf01
Richard Goulter <richard.goulter@gmail.com>
2015-04-11 02:11:48 +0800

    Refactor TypeInference tests, to improve readability.

--- src/test/scala/edu/nus/worksheet/instrumentor/test/TypeInferenceSpec.scala
+++ src/test/scala/edu/nus/worksheet/instrumentor/test/TypeInferenceSpec.scala
@@ -7,35 +7,40 @@ import edu.nus.worksheet.instrumentor.TypeInference.inferType;
 import edu.nus.worksheet.instrumentor.TypeInference.dummyGlobalScopeFor;
 
 class TypeInferenceSpec extends FlatSpec {
+  def assertInference(result : Any, program : String, expr : String) {
+    val scope = if (program != null) dummyGlobalScopeFor(program) else null;
+    val inferredType = inferType(scope, expr);
+    assertResult(result)(inferredType);
+  }
 
   "Type inference" should "work for simple constants" in {
-    assertResult(PrimitiveType(null, "int"))(inferType(null, "5"));
-    assertResult(PrimitiveType(null, "double"))(inferType(null, "5.34"));
-    assertResult(PrimitiveType(null, "char"))(inferType(null, "'x'"));
+    assertInference(PrimitiveType(null, "int"), null, "5");
+    assertInference(PrimitiveType(null, "double"), null, "5.34");
+    assertInference(PrimitiveType(null, "char"), null, "'x'");
   }
 
   it should "work for primary expressions" in {
-    assertResult(PrimitiveType(null, "string"))(inferType(null, "\"Abc\""));
-    assertResult(PrimitiveType(null, "string"))(inferType(null, "\"Abc\" \"def\""));
+    assertInference(PrimitiveType(null, "string"), null, "\"Abc\"");
+    assertInference(PrimitiveType(null, "string"), null, "\"Abc\" \"def\"");
 
-    assertResult(PrimitiveType(null, "int"))(inferType(null, "(5)"));
+    assertInference(PrimitiveType(null, "int"), null, "(5)");
 
     // If infering type of a variable, return the same id.
-    assertResult(PrimitiveType("x", "int"))(inferType(dummyGlobalScopeFor("int x;"), "x"));
+    assertInference(PrimitiveType("x", "int"), "int x;", "x");
   }
 
   it should "infer postfix expressions" in {
-    assertResult(PrimitiveType("x[x_0]", "int"))(inferType(dummyGlobalScopeFor("int x[2] = {1,2};"), "x[0]"));
-    assertResult(PrimitiveType("s.x", "int"))(inferType(dummyGlobalScopeFor("struct {int x;} s;"), "s.x"));
-    assertResult(PrimitiveType("(*p).x", "int"))(inferType(dummyGlobalScopeFor("struct S {int x;} s; struct S *p = &s;"), "p->x"));
-    assertResult(PrimitiveType("i", "int"))(inferType(dummyGlobalScopeFor("int i;"), "i++"));
+    assertInference(PrimitiveType("x[x_0]", "int"), "int x[2] = {1,2};", "x[0]");
+    assertInference(PrimitiveType("s.x", "int"), "struct {int x;} s;", "s.x");
+    assertInference(PrimitiveType("(*p)", "int"), "struct S {int x;} s; struct S *p = &s;", "p->x");
+    assertInference(PrimitiveType("i", "int"), "int i;", "i++");
   }
 
   it should "infer infix expressions" in {
-    assertResult(PrimitiveType("i", "int"))(inferType(dummyGlobalScopeFor("int i;"), "++i"));
-    assertResult(PrimitiveType("i", "int"))(inferType(dummyGlobalScopeFor("int i;"), "!i"));
-    assertResult(PointerType(null, PrimitiveType("i", "int")))(inferType(dummyGlobalScopeFor("int i;"), "&i"));
-    assertResult(PrimitiveType("(*p)", "int"))(inferType(dummyGlobalScopeFor("int *p;"), "*p"));
-    assertResult(PrimitiveType(null, "size_t"))(inferType(dummyGlobalScopeFor("int i;"), "sizeof p"));
+    assertInference(PrimitiveType("i", "int"), "int i;", "++i");
+    assertInference(PrimitiveType("i", "int"), "int i;", "!i");
+    assertInference(PointerType(null, PrimitiveType("i", "int")), "int i;", "&i");
+    assertInference(PrimitiveType("(*p)", "int"), "int *p;", "*p");
+    assertInference(PrimitiveType(null, "size_t"), "int i;", "sizeof p");
   }
 }
