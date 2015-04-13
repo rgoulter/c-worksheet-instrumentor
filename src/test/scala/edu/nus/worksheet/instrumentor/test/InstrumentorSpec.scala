855ff3441be9465d13bafe2d80778b800e09cbfa
Richard Goulter <richard.goulter@gmail.com>
2015-04-14 00:57:21 +0800

    Add (failing) tests for postfix expr stmt.

--- src/test/scala/edu/nus/worksheet/instrumentor/test/InstrumentorSpec.scala
+++ src/test/scala/edu/nus/worksheet/instrumentor/test/InstrumentorSpec.scala
@@ -174,6 +174,115 @@ int main(int argc, char* argv) { // Line 03
     assert(errors.isEmpty, "No warnings");
   }
 
+  it should "not produce warnings when instrumenting (postfix array expression statements)" in {
+    // Bug was that would get warnings
+    // introduced for instrumenting assignments.
+    val inputProgram = """#include <stdio.h>
+
+int main(int argc, char* argv) { // Line 03
+  int arr[3] = {3,4,5};
+
+  // Various postfix expressions
+  arr[1];
+}""";
+    val instrumentedProgram = Instrumentor.instrument(inputProgram);
+
+    val prog = new CProgram(instrumentedProgram);
+
+    val (warnings, errors) = prog.compile();
+
+    assert(warnings.isEmpty, "No warnings");
+    assert(errors.isEmpty, "No warnings");
+  }
+
+  it should "not produce warnings when instrumenting (postfix fn call expression statements)" in {
+    // Bug was that would get warnings
+    // introduced for instrumenting assignments.
+    val inputProgram = """#include <stdio.h>
+
+int f() {
+  return 5;
+}
+
+int main(int argc, char* argv) { // Line 03
+  // Various postfix expressions
+  f();
+}""";
+    val instrumentedProgram = Instrumentor.instrument(inputProgram);
+
+    val prog = new CProgram(instrumentedProgram);
+
+    val (warnings, errors) = prog.compile();
+
+    assert(warnings.isEmpty, "No warnings");
+    assert(errors.isEmpty, "No warnings");
+  }
+
+  it should "not produce warnings when instrumenting (postfix struct expression statements)" in {
+    // Bug was that would get warnings
+    // introduced for instrumenting assignments.
+    val inputProgram = """#include <stdio.h>
+
+int main(int argc, char* argv) { // Line 03
+  struct S {int x; int y;} s1 = {3, 4};
+  struct S *ps1 = &s1;
+
+  // Various postfix expressions
+  s1.x;
+  ps1->y;
+}""";
+    val instrumentedProgram = Instrumentor.instrument(inputProgram);
+
+    val prog = new CProgram(instrumentedProgram);
+
+    val (warnings, errors) = prog.compile();
+
+    assert(warnings.isEmpty, "No warnings");
+    assert(errors.isEmpty, "No warnings");
+  }
+
+  ignore should "not produce warnings when instrumenting (postfix incr/decr expression statements)" in {
+    // Bug was that would get warnings
+    // introduced for instrumenting assignments.
+    val inputProgram = """#include <stdio.h>
+
+int main(int argc, char* argv) { // Line 03
+  int i = 10;
+
+  // Various postfix expressions
+  i++;
+  i++;
+  i--;
+}""";
+    val instrumentedProgram = Instrumentor.instrument(inputProgram);
+
+    val prog = new CProgram(instrumentedProgram);
+
+    val (warnings, errors) = prog.compile();
+
+    assert(warnings.isEmpty, "No warnings");
+    assert(errors.isEmpty, "No warnings");
+  }
+
+  it should "not produce warnings when instrumenting (postfix compound literal expression statements)" in {
+    // Bug was that would get warnings
+    // introduced for instrumenting assignments.
+    val inputProgram = """#include <stdio.h>
+
+int main(int argc, char* argv) { // Line 03
+  // Various postfix expressions
+  (int[3]) {7,6,8};
+}""";
+    val instrumentedProgram = Instrumentor.instrument(inputProgram);
+
+    val prog = new CProgram(instrumentedProgram);
+
+    val (warnings, errors) = prog.compile();
+
+    assert(warnings.isEmpty, "No warnings");
+    assert(errors.isEmpty, "No warnings");
+  }
+
   it should "be able to instrument an array declaration where leftmost dimension not specified in arr decl." in {
     val inputProgram = """#include <stdio.h>
 int main(int argc, char **argv) { // Line 02
