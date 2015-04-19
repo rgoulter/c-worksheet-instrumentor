562af854b54baf8b7d061a7639b2562db67c2722
Richard Goulter <richard.goulter@gmail.com>
2015-04-20 00:35:00 +0800

    FindCompiler looks for `gcc.exe` on Windows, not `cc`.
    *Should* work on Windows, since nothing else is OS-specific,
    as far as I can tell.

--- src/main/scala/edu/nus/worksheet/instrumentor/FindCompiler.scala
+++ src/main/scala/edu/nus/worksheet/instrumentor/FindCompiler.scala
@@ -8,11 +8,17 @@ object FindCompiler {
     val path = System.getenv("PATH")
     val pathEls = path.split(File.pathSeparator);
 
+    val ccName = if (System.getProperty("os.name").contains("win"))
+                   // On Windows, we expect Mingw GCC to be on the PATH
+                   "gcc.exe";
+                 else
+                   "cc";
+
     def maybeCC(f : String) : Option[String] = {
       val folder = new File(f);
 
       if (folder.isDirectory()) {
-        val cc = new File(folder, "cc");
+        val cc = new File(folder, ccName);
 
         if (cc.exists() && cc.canExecute())
           return Some(cc.getAbsolutePath());
@@ -22,14 +28,14 @@ object FindCompiler {
         return None;
       }
     }
-    
+
     val compilers = pathEls.flatMap(maybeCC);
     if (compilers.isEmpty)
       throw new RuntimeException("Expected to find CC on PATH");
     else
       return compilers(0);
   }
-  
+
   def main(args : Array[String]) : Unit = {
     println("CC: " + findCompilerOnPath());
   }
