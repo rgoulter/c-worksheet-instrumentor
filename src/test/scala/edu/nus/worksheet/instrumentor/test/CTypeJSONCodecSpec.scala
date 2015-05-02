ac7cd3d4b8ee158193dbb3fa6b637baf31904825
Richard Goulter <richard.goulter@gmail.com>
2015-05-03 00:48:53 +0800

    Add tests for CType JSON encode/decode.
    Tests which are explicitly ignored would fail.

---
+++ src/test/scala/edu/nus/worksheet/instrumentor/test/CTypeJSONCodecSpec.scala
@@ -0,0 +1,134 @@
+package edu.nus.worksheet.instrumentor.test
+
+import org.scalatest._
+import edu.nus.worksheet.instrumentor._
+import CTypeCodec.{ CTypeEncodeJson, CTypeDecodeJson };
+import argonaut._, Argonaut._;
+
+class CTypeJSONCodecSpec extends FlatSpec {
+  def assertIdempotentEncodeAndDecode(ct : CType) {
+    val ctJson = ct.asJson;
+    val wire : String = ctJson.nospaces;
+
+    wire.decodeOption[CType] match {
+      case Some(decodedCt) =>
+        assertResult(ct)(decodedCt);
+      case None =>
+        fail(s"Couldn't decode for CType $ct");
+    }
+  }
+
+  "CType JSON Codec" should "be idempotent for primitive types" in {
+    val ct = new PrimitiveType("x", "int");
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  // Assume that if null/None id for PrimitiveType is handled correctly,
+  // then it's handled correctly for other CTypes.
+  it should "be idempotent for primitive types (with null id)" in {
+    val ct = new PrimitiveType(null : String, "int");
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  it should "be idempotent for array types" in {
+    // n.b. w/ ArrayType; id, idx, n are OptionTypes.
+
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new ArrayType("y", "idx", "n", of);
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  it should "be idempotent for array types (with null idx, n)" in {
+    // n.b. w/ ArrayType; id, idx, n are OptionTypes.
+
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new ArrayType(null : String, null, null, of);
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  it should "be idempotent for pointer types" in {
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new PointerType("y", of);
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  it should "be idempotent for struct types" in {
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new StructType("y", "struct", "S", Seq(of));
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  it should "be idempotent for struct types (with no members)" in {
+    // id of `of` not important here, (no semantic meaning).
+    val ct = new StructType("y", "struct", "S", Seq());
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  it should "be idempotent for struct types (with null tag)" in {
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new StructType("y", "struct", null, Seq(of));
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  ignore should "be idempotent for enum types" in {
+    val ct = new EnumType("x", "E", Seq("A", "B"));
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  ignore should "be idempotent for enum types (with null tag)" in {
+    val ct = new EnumType("x", null, Seq("A", "B"));
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  it should "be idempotent for function types" in {
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new FunctionType("y", of, Seq(of));
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  it should "be idempotent for function types (with no parameters)" in {
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new FunctionType("y", of, Seq());
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  // CTypeToDeclaration, TypeInference somewhat ambivalent whether to have `null` as ctype
+  //  for returnType of functions which return void.
+  // So, for now, have specs/tests for both.
+
+  ignore should "be idempotent for function types (with void return type)" in {
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new FunctionType("y", VoidType(), Seq(of));
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  // amazingly, this passes.
+  it should "be idempotent for function types (with null return type)" in {
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new FunctionType("y", null, Seq(of));
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  it should "be idempotent for function types (with vararg type parameter)" in {
+    // id of `of` not important here, (no semantic meaning).
+    val of = new PrimitiveType("x", "int");
+    val ct = new FunctionType("y", of, Seq(of, VarArgType()));
+    assertIdempotentEncodeAndDecode(ct);
+  }
+
+  // forward decln.
+  it should "be idempotent for ForwardDecl'n types" in {
+    // id of `of` not important here, (no semantic meaning).
+    val ct = ForwardDeclarationType(Some("fd"), "S");
+    assertIdempotentEncodeAndDecode(ct);
+  }
+}
