7ce434266e6a89b4ac9d178cd69d20ed8d7c3b86
Richard Goulter <richard.goulter@gmail.com>
2024-05-15 20:51:09 +0700

    java: add UsesGCC, UsesClang tags for ScalaTest

---
+++ src/test/java/edu/nus/worksheet/tags/UsesClang.java
@@ -0,0 +1,9 @@
+package edu.nus.worksheet.tags;
+
+import java.lang.annotation.*;
+import org.scalatest.TagAnnotation;
+
+@TagAnnotation
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface UsesClang {}
