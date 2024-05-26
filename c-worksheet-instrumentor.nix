0783f93221d1edae2d7ddd18f98d7488aa19894d
Richard Goulter <richard.goulter@gmail.com>
2024-05-26 18:36:38 +0700

    nix: update gradle fod hash for scala3

--- c-worksheet-instrumentor.nix
+++ c-worksheet-instrumentor.nix
@@ -50,7 +50,7 @@ stdenv.mkDerivation (finalAttrs: {
     installPhase = ''
       runHook preInstall
 
-      find $GRADLE_USER_HOME/caches/modules-2 -type f -regex '.*\.\(jar\|pom\)' \
+      find $GRADLE_USER_HOME/caches/modules-2 -type f -regex '.*\.\(jar\|pom\|module\)' \
         | perl -pe 's#(.*/([^/]+)/([^/]+)/([^/]+)/[0-9a-f]{30,40}/([^/\s]+))$# ($x = $2) =~ tr|\.|/|; "install -Dm444 $1 \$out/$x/$3/$4/$5" #e' \
         | sh
 
@@ -60,7 +60,7 @@ stdenv.mkDerivation (finalAttrs: {
     # Remember to update FOD outputHash whenever build.gradle dependencies change
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
-    outputHash = "sha256-DxthMN/wRt1kpqD89qkCQ5WlYJiNnyQssSdRvxEfRkM=";
+    outputHash = "sha256-xw7DoUKkGYsGxVHxgl3qhWjQRanHvGDyP2jVt3ynjGw=";
   };
 
   nativeBuildInputs = [
