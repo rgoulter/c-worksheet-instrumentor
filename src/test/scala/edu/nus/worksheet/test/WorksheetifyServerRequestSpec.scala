14b15c532b3f8279fb632217419c052ba2b76f18
Richard Goulter <richard.goulter@gmail.com>
2024-05-26 19:34:08 +0700

    spec: add WorksheetifyServer Request spec

---
+++ src/test/scala/edu/nus/worksheet/test/WorksheetifyServerRequestSpec.scala
@@ -0,0 +1,24 @@
+package edu.nus.worksheet.test
+
+import argonaut.Argonaut.*
+import argonaut.Parse
+import argonaut.Json
+
+import org.scalatest.*
+import flatspec.*
+
+import edu.nus.worksheet.WorksheetifyServer.Request
+import edu.nus.worksheet.WorksheetifyServer.requestCodecJson
+
+class WorksheetifyServerRequestSpec extends AnyFlatSpec {
+
+  "WorksheetifyServer.Request" should "decode JSON" in {
+    val expectedReq = Request("file", "./hello.c", "json-outputlist", None, None);
+
+    val requestJson = """{"inputtype":"file", "input":"./hello.c", "outputtype":"json-outputlist"}""";
+    val actualReq = Parse.decodeOption[Request](requestJson.toString).get;
+
+    assertResult(expectedReq)(actualReq);
+  }
+
+}
