d17d1932cf25ac6727ebf4eddaf312f63edb5504
Richard Goulter <richard.goulter@gmail.com>
2024-05-30 13:59:10 +0700

    update nix files to use gradle2nix v2

--- shell.nix
+++ shell.nix
@@ -1,7 +1,9 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+{
+  pkgs ? import <nixpkgs> {},
+  gradle2nix ? import (fetchTarball "https://github.com/rgoulter/gradle2nix/archive/v2--always-set-gradle-user-home.tar.gz") {},
+}:
 pkgs.mkShell {
-  inputsFrom = [(pkgs.callPackage ./c-worksheet-instrumentor.nix {})];
+  inputsFrom = [(pkgs.callPackage ./default.nix {inherit pkgs gradle2nix;})];
   packages = [
     pkgs.metals
     pkgs.scalafix
